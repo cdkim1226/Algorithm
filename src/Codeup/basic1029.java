f1a20fbdf3211b9092f869631363a207e1b33bbd
cdkim1226 <cdkim1226@gmail.com>
2020-10-01 15:01:20 +0900

    소수점 뒷자리 11자리까지 출력하기

---
+++ src/Codeup/basic1029.java
@@ -0,0 +1,15 @@
+package Codeup;
+
+import java.util.Scanner;
+
+public class basic1029 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		double n = sc.nextDouble();
+		System.out.printf("%.11f",n);
+	}
+	
+
+}
