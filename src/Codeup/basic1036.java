7cfb6cac433403ba369c34688e49d086072bbd7b
cdkim1226 <cdkim1226@gmail.com>
2020-10-01 15:19:15 +0900

    아스키코드를 입력받아 숫자로 출력

---
+++ src/Codeup/basic1036.java
@@ -0,0 +1,17 @@
+package Codeup;
+
+import java.util.Scanner;
+
+public class basic1036 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		char n = sc.nextLine().charAt(0);
+		sc.close();
+		
+		int result = (int)n;
+		System.out.println(result);
+	}
+	
+
+}
