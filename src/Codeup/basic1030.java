2f327a7e7ac09976bfc7e900f5f863e462e44d1a
cdkim1226 <cdkim1226@gmail.com>
2020-10-01 15:02:27 +0900

    10진 정수 8진수로 출력

---
+++ src/Codeup/basic1030.java
@@ -0,0 +1,16 @@
+package Codeup;
+
+import java.util.Scanner;
+
+public class basic1030 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		String result = Integer.toOctalString(n);
+		System.out.println(result);
+	}
+	
+
+}
