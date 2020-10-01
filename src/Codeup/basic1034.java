cf3835b1340188f1cc2077075d36ee581206a84d
cdkim1226 <cdkim1226@gmail.com>
2020-10-01 15:04:24 +0900

    8진 정수를 10진수로 출력

---
+++ src/Codeup/basic1034.java
@@ -0,0 +1,16 @@
+package Codeup;
+
+import java.util.Scanner;
+
+public class basic1034 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String n = sc.nextLine();
+		int result = Integer.valueOf(n,8);
+		System.out.printf("%d",result);
+	}
+	
+
+}
