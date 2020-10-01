dcbee71ee99d7c7c4c73b25bf0b253537d2668d9
cdkim1226 <cdkim1226@gmail.com>
2020-10-01 15:03:01 +0900

    10진 정수를 16진수로 출력

---
+++ src/Codeup/basic1032.java
@@ -0,0 +1,16 @@
+package Codeup;
+
+import java.util.Scanner;
+
+public class basic1032 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		String result = Integer.toHexString(n);
+		System.out.println(result);
+	}
+	
+
+}
