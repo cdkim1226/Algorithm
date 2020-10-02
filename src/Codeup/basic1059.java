91e54c50db112326d9def54a09bed0d72956e101
cdkim1226 <cdkim1226@gmail.com>
2020-10-02 23:48:15 +0900

    비트 단위 연산자 ~는 비트단위 반대로 출력한다

---
+++ src/Codeup/basic1059.java
@@ -0,0 +1,15 @@
+package Codeup;
+
+import java.util.Scanner;
+
+public class basic1059 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = ~a;
+		sc.close();
+			System.out.println(b);
+	}
+	
+}
