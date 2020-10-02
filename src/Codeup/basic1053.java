b4d703e950afd6bcf8718dc6bc3efa3cb724cf93
cdkim1226 <cdkim1226@gmail.com>
2020-10-02 23:45:50 +0900

    비교 연산 a 가 1이면 0 아니면 1 출력

---
+++ src/Codeup/basic1053.java
@@ -0,0 +1,15 @@
+package Codeup;
+
+import java.util.Scanner;
+
+public class basic1053 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		sc.close();
+			System.out.println(a == 1? 0 : 1);
+	}
+	
+
+}
