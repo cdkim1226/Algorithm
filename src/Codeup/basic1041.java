cb50e7d375251a1c1940811b40853a3fe158fbec
cdkim1226 <cdkim1226@gmail.com>
2020-10-02 23:40:27 +0900

    알파벳 입력 받아 다음 알파벳으로 출력

---
+++ src/Codeup/basic1041.java
@@ -0,0 +1,15 @@
+package Codeup;
+
+import java.util.Scanner;
+
+public class basic1041 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		char n = sc.nextLine().charAt(0);
+		sc.close();
+		System.out.print((char)(n+1));
+	}
+	
+
+}
