39e2aa166c0f134381b5ace38edf2011cf3dca69
cdkim1226 <cdkim1226@gmail.com>
2020-10-02 23:47:27 +0900

    a 가 1이 아니거나 b가 1이 아니면 1 맞으면 0 출력

---
+++ src/Codeup/basic1058.java
@@ -0,0 +1,16 @@
+package Codeup;
+
+import java.util.Scanner;
+
+public class basic1058 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		sc.close();
+			System.out.println(a != 1 && b != 1? 1 : 0);
+	}
+	
+
+}
