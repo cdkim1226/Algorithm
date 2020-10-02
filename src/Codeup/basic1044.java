e11bd96cdc8900e0b50769832260f5595a3ab2aa
cdkim1226 <cdkim1226@gmail.com>
2020-10-02 23:41:26 +0900

    입력받은 정수값(long type) 1을 더해 출력

---
+++ src/Codeup/basic1044.java
@@ -0,0 +1,17 @@
+package Codeup;
+
+import java.util.Scanner;
+
+public class basic1044 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long a = sc.nextLong();
+		sc.close();
+		
+		long result = a+1;
+		System.out.println(result);
+	}
+	
+
+}
