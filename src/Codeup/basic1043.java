41e7c734d46becb04bc63f9ff8c055251aca863c
cdkim1226 <cdkim1226@gmail.com>
2020-10-02 23:41:00 +0900

    정수 a,b 나누고 남은값 출력

---
+++ src/Codeup/basic1043.java
@@ -0,0 +1,18 @@
+package Codeup;
+
+import java.util.Scanner;
+
+public class basic1043 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		sc.close();
+		
+		int result = a%b;
+		System.out.println(result);
+	}
+	
+
+}
