77b8f98994ec1a140fc8a0f6af1a482eb5407cd0
cdkim1226 <cdkim1226@gmail.com>
2020-10-02 23:39:26 +0900

    입력받은 정수를 반대(음,양)로 출력

---
+++ src/Codeup/basic1040.java
@@ -0,0 +1,18 @@
+package Codeup;
+
+import java.util.Scanner;
+
+public class basic1040 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		sc.close();
+		
+		System.out.println(-a);
+		
+	}
+	
+
+}
