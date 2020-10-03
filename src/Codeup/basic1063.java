a4c034c677c891648a50c8dbd11f3b2096c2c179
cdkim1226 <cdkim1226@gmail.com>
2020-10-03 19:25:31 +0900

    a 보다 b가 크면 a를 출력 아니면 b를 출력

---
+++ src/Codeup/basic1063.java
@@ -0,0 +1,14 @@
+package Codeup;
+
+import java.util.Scanner;
+public class basic1063 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		sc.close();
+			System.out.println(a > b? a : b);
+	}
+	
+}
