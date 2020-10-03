25aa7d3951d5b64b16ab6df35eefa188c843fae3
cdkim1226 <cdkim1226@gmail.com>
2020-10-03 19:29:29 +0900

    무작위 정수를 입력했을 때 0이 나올때까지 출력

---
+++ src/Codeup/basic1071.java
@@ -0,0 +1,18 @@
+package Codeup;
+
+import java.util.Scanner;
+
+public class basic1071 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+			int a = sc.nextInt();
+			if(a == 0) {
+				break;
+			}
+			System.out.println(a);
+		}
+	}
+}
