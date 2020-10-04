8ab18f547bb08e9278b314a4177285aff306c069
cdkim1226 <cdkim1226@gmail.com>
2020-10-04 19:43:20 +0900

    r,g,b 3가지 색깔의 종류 수를 입력 받아 경우의 수 출력하기

---
+++ src/Codeup/basic1084.java
@@ -0,0 +1,24 @@
+package Codeup;
+
+import java.util.Scanner;
+
+public class basic1084 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int r = sc.nextInt();
+		int g = sc.nextInt();
+		int b = sc.nextInt();
+		int count = 0;
+		sc.close();
+		for(int i=0; i<r; i++) {
+			for(int j=0; j<g; j++) {
+				for(int k=0; k<b; k++) {
+					System.out.println(i+" "+j+" "+k);
+					count++;
+				}
+			}
+		}
+		System.out.print(count);
+	}
+}
