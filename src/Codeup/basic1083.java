6264918d0aa6af31c126acba2d75712c078da349
cdkim1226 <cdkim1226@gmail.com>
2020-10-05 01:35:41 +0900

    스캐너 닫아주기

--- src/Codeup/basic1083.java
+++ src/Codeup/basic1083.java
@@ -7,6 +7,7 @@ public class basic1083 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
+		sc.close();
 		for(int i=1; i<=n; i++) {
 			if(i%3 == 0) {
 				System.out.print("X ");
